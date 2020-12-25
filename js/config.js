6d1bdeac2abe4bd0bebdb4ce83700fc0d0a432aa
n0vel1a-off <76596006+n0vel1a-off@users.noreply.github.com>
2020-12-25 22:12:28 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,45 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 1.5, 
+
+  // Заголовок
+  title: 'n0vel1a',
+
+  // Социальные сети
+  social: {
+    telegram: '@yttaxion',
+    steam: 'n0vel1a',
+    vk: 'n0vel1a', 
+  },
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#f5323c',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#f5323c',
+  // Цвет тени кнопки play
+  playButtonShadowColor: 'rgba(255, 0, 55, 5.0)',
+
+
+  // Цвет заголовка
+  titleColor: '#FF0000',
+  // Цвет тени загаловка
+  titleShadowColor: 'rgba(255, 0, 55, 0.6)',
+
+
+  // Текст копирайта
+  copyrightText: 'dev - ghoul',
+  // Цвет копирайта
+  copyrightTextColor: '#f5323c',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#FF0000',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.2)',
+}
